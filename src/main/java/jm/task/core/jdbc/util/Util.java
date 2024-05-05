8896105da7a6fa4f4c643495267446310ed78e65
Yury Goodyma <goodislav@gmail.com>
2024-05-05 22:18:09 +0300

    add Hibernate connection to Util class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,17 +3,30 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Properties;
+
+import jm.task.core.jdbc.model.User;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
 public class Util {
     // реализуйте настройку соединения с БД
+    private static SessionFactory sessionFactory;
     private static final Logger logger = Logger.getLogger(Util.class.getName());
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/users";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "bB?QL+4xY?$4QiD";
 
+    //    JDBC connection
     public static Connection getConnection() {
         Connection connection = null;
         try {
@@ -24,5 +37,37 @@ public class Util {
             logger.log(Level.SEVERE, "Connection has ERROR", e);
         }
         return connection;
+    }
+
+    //    Hibernate connection
+    public static SessionFactory getSessionFactory() {
+            if (sessionFactory == null) {
+                try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, DRIVER);
+                settings.put(Environment.URL, URL + "?useSSL=false");
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                    logger.log(Level.INFO, "Connection with Hibernate is OK");
+            } catch (HibernateException e) {
+                    logger.log(Level.SEVERE, "Connection with Hibernate has ERROR", e);
+                }
+            }
+        return sessionFactory;
     }
 }

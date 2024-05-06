367c77abcbe764830f48a3d1dbdb46c3f76868af
Yury Goodyma <goodislav@gmail.com>
2024-05-06 21:42:13 +0300

    delete unused exception from UserDao and edit UserServiceImpl as Hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,35 +1,37 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class UserServiceImpl implements UserService {
+    UserDao userDaoHibernate = new UserDaoHibernateImpl();
     public void createUsersTable() {
-        new UserDaoJDBCImpl().createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
-        new UserDaoJDBCImpl().dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        new UserDaoJDBCImpl().saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
         Logger logger = Logger.getLogger(User.class.getName());
         String message = String.format("User с именем — %s добавлен в базу данных", name);
         logger.info(message);
     }
 
     public void removeUserById(long id) {
-        new UserDaoJDBCImpl().removeUserById(id);
+        userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return new UserDaoJDBCImpl().getAllUsers();
+        return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        new UserDaoJDBCImpl().cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
     }
 }
